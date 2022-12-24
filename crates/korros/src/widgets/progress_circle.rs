25355b6b8d344e77860922d489e48cb701a6c036
Cédric Eberhardt <hello+code@cedeber.fr>
2022-12-24 17:42:31 +0100

    Add aria attributes to ProgressCircle

--- crates/korros/src/widgets/progress_circle.rs
+++ crates/korros/src/widgets/progress_circle.rs
@@ -1,14 +1,14 @@
 use super::ViewComponent;
-use crate::utils::element::create_svg_element;
+use crate::utils::element::{create_element, create_svg_element};
 use futures_signals::signal::{Signal, SignalExt};
 use std::f32::consts::PI;
 use wasm_bindgen::UnwrapThrowExt;
 use wasm_bindgen_futures::spawn_local;
-use web_sys::{Node, SvgAnimateTransformElement, SvgCircleElement, SvgsvgElement};
+use web_sys::{HtmlDivElement, Node, SvgAnimateTransformElement, SvgCircleElement, SvgsvgElement};
 
 #[derive(Clone)]
 pub struct ProgressCircle {
-	container: SvgsvgElement,
+	container: HtmlDivElement,
 	progress_circle: SvgCircleElement,
 	circumference: f32,
 }
@@ -20,8 +20,10 @@ impl ViewComponent for ProgressCircle {
 }
 
 impl ProgressCircle {
+	// TODO Extract size and is_indeternimate. Add label instead.
 	pub fn new(size: f32, is_indeterminate: bool) -> Self {
-		let container: SvgsvgElement = create_svg_element("svg");
+		let progress_element: HtmlDivElement = create_element("div");
+		let loading: SvgsvgElement = create_svg_element("svg");
 		let background_circle: SvgCircleElement = create_svg_element("circle");
 		let progress_circle: SvgCircleElement = create_svg_element("circle");
 		let transform: SvgAnimateTransformElement = create_svg_element("animateTransform");
@@ -35,13 +37,24 @@ impl ProgressCircle {
 
 		let size_str = &(center * 2.0).to_string();
 
-		container.set_attribute("width", size_str).unwrap_throw();
-		container.set_attribute("height", size_str).unwrap_throw();
-		container
+		progress_element
+			.set_attribute("role", "progressbar")
+			.unwrap_throw();
+		progress_element
+			.set_attribute("aria-label", "Loading")
+			.unwrap_throw();
+		progress_element
+			.set_attribute("class", "korros__progress_circle")
+			.unwrap_throw();
+
+		loading.set_attribute("aria-hidden", "true").unwrap_throw();
+		loading.set_attribute("width", size_str).unwrap_throw();
+		loading.set_attribute("height", size_str).unwrap_throw();
+		loading
 			.set_attribute("viewBox", &format!("0 0 {size_str} {size_str}"))
 			.unwrap_throw();
-		container.set_attribute("fill", "none").unwrap_throw();
-		container
+		loading.set_attribute("fill", "none").unwrap_throw();
+		loading
 			.set_attribute("stroke-width", &stroke_width.to_string())
 			.unwrap_throw();
 
@@ -58,7 +71,7 @@ impl ProgressCircle {
 			.set_attribute("stroke", "currentColor")
 			.unwrap_throw();
 		background_circle
-			.set_attribute("opacity", "0.13")
+			.set_attribute("opacity", "0.2")
 			.unwrap_throw();
 
 		progress_circle
@@ -103,11 +116,13 @@ impl ProgressCircle {
 
 		progress_circle.append_child(&transform).unwrap_throw();
 
-		container.append_child(&background_circle).unwrap_throw();
-		container.append_child(&progress_circle).unwrap_throw();
+		loading.append_child(&background_circle).unwrap_throw();
+		loading.append_child(&progress_circle).unwrap_throw();
+
+		progress_element.append_child(&loading).unwrap_throw();
 
 		ProgressCircle {
-			container,
+			container: progress_element,
 			progress_circle,
 			circumference,
 		}
@@ -124,6 +139,12 @@ impl ProgressCircle {
 				.progress_circle
 				.set_attribute("stroke-dashoffset", &offset.to_string())
 				.unwrap_throw();
+
+			circle_clone
+				.container
+				.set_attribute("aria-valuenow", &value.to_string())
+				.unwrap_throw();
+
 			async {}
 		});
 
