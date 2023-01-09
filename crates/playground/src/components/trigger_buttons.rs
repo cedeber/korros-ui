89546770844ba5a8896360125d327860e907a95b
Cédric Eberhardt <hello+code@cedeber.fr>
2023-01-10 00:54:58 +0100

    more examples

--- crates/playground/src/components/trigger_buttons.rs
+++ crates/playground/src/components/trigger_buttons.rs
@@ -1,6 +1,7 @@
 use futures_signals::signal::Mutable;
 use korros::widgets::{
 	stack::HStack,
+	text::Text,
 	toggle::Toggle,
 	trigger_button::{TriggerButton, TriggerButtonIntent},
 	Widget,
@@ -23,24 +24,35 @@ impl Default for Store {
 pub fn trigger_buttons() -> impl Widget {
 	let store = Store::default();
 
+	let reactive_text = Mutable::new("Press a button!");
+	let text = Text::new_signal(reactive_text.signal());
+
+	let clone_signal = reactive_text.clone();
 	let button_delete = TriggerButton::new("Delete", "delete")
 		.intent(TriggerButtonIntent::Danger)
 		.disabled_signal(store.disabled.signal())
-		.show_label_signal(store.show_label.signal());
+		.show_label_signal(store.show_label.signal())
+		.on_press(move |_| clone_signal.set("Pressed Button 1"));
 
+	let clone_signal = reactive_text.clone();
 	let button2 = TriggerButton::new("Save", "save")
 		.disabled_signal(store.disabled.signal())
-		.show_label_signal(store.show_label.signal());
+		.show_label_signal(store.show_label.signal())
+		.on_press(move |_| clone_signal.set("Pressed Button 2"));
 
+	let clone_signal = reactive_text.clone();
 	let button3 = TriggerButton::new("Save", "save")
 		.intent(TriggerButtonIntent::Outlined)
 		.disabled_signal(store.disabled.signal())
-		.show_label_signal(store.show_label.signal());
+		.show_label_signal(store.show_label.signal())
+		.on_press(move |_| clone_signal.set("Pressed Button 3"));
 
+	let clone_signal = reactive_text.clone();
 	let button4 = TriggerButton::new("Save", "save")
 		.intent(TriggerButtonIntent::Active)
 		.disabled_signal(store.disabled.signal())
-		.show_label_signal(store.show_label.signal());
+		.show_label_signal(store.show_label.signal())
+		.on_press(move |_| clone_signal.set("Pressed Button 4"));
 
 	let switch_disabled = Toggle::new_signal(store.disabled.signal())
 		.on_change(move |is_checked| store.disabled.set(is_checked));
@@ -56,4 +68,5 @@ pub fn trigger_buttons() -> impl Widget {
 		.child(&button2)
 		.child(&button3)
 		.child(&button4)
+		.child(&text)
 }
